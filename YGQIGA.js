b6bcf9de1bda73972fd8b041bf858b0fb324a168
Deepak Singh <46400817+DeepakSingh260@users.noreply.github.com>
2020-06-20 13:12:05 +0530

    Update YGQIGA.js

--- YGQIGA.js
+++ YGQIGA.js
@@ -1,1 +1,9 @@
 alert('this site is being designed to solve your problems')
+function openSlideMenu() {
+	document.getElementById('side-menu').style.width='250px';
+	
+}      
+function closeSlideMenu() {
+	document.getElementById('side-menu').style.width='0px';
+	
+}   
