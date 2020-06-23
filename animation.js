9e605d0c6f133f34730f3be8fa9be3d10b129273
Deepak Singh <46400817+DeepakSingh260@users.noreply.github.com>
2020-06-23 18:45:37 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -5,6 +5,6 @@ const MySite= document.querySelector('.MySite');
 const slider= document.querySelector('.slider');
 
 const t1= new TimelineMax();
-t1.fromTo(background,1,{height:'0%'},{ height:'80%',ease : Power2.easeInOut})
+t1.fromTo(background,1,{height:'0%'},{ height:'90%',ease : Power2.easeInOut})
 .fromTo(background, 1.2 ,{width: '100%'},{width : '80%' , ease : Power2.easeInOut})
 .fromTo(slider, 1.2 , {x : "-100%"} , {x : '0%' , ease : Power2.easeInOut});
