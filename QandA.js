33bcf071976c086084d34f87ff8b2d3a2cac38f5
Deepak Singh <46400817+DeepakSingh260@users.noreply.github.com>
2020-06-26 12:14:20 +0530

    Add files via upload

---
+++ QandA.js
@@ -0,0 +1,8 @@
+
+function openSlideMenu() {
+	document.getElementById('side-menu').style.width='250px';
+	
+}      
+function closeSlideMenu() {
+	document.getElementById('side-menu').style.width='0px';
+}   
